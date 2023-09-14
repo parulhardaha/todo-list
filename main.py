38a016f17906e1d7d64de915c0bcb258d99b55d9
parulhardaha <phardaha5@gmail.com>
2023-09-14 22:48:42 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask ,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/products')
+def products():
+    return 'My Page'
+
+if __name__=="__main__":
+ app.run(debug=True)
+
+   
